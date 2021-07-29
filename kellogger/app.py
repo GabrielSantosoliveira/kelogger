df2f7eee855c5e83738a6756e2c25c8ca91e8215
GabrielSantosoliveira <72681794+GabrielSantosoliveira@users.noreply.github.com>
2021-07-29 01:10:19 -0300

    Add files via upload

---
+++ kellogger/app.py
@@ -0,0 +1,123 @@
+import pyautogui 
+import keyboard
+pyautogui.alert('començando o keylogger  usando keyboard ')
+
+
+def keylogger():
+    while True:
+
+        while True:
+            if keyboard.is_pressed('q'):
+                print('q')
+                break
+            elif keyboard.is_pressed('a'):
+                print('a')
+                break
+            elif keyboard.is_pressed('b'):
+                print('b')
+                break
+            elif keyboard.is_pressed('c'):
+                print('c')
+                break
+            elif keyboard.is_pressed('d'):
+                print('d')
+                break
+            elif keyboard.is_pressed('e'):
+                print('e')
+                break
+            elif keyboard.is_pressed('f'):
+                print('f')
+                break
+            elif keyboard.is_pressed('g'):
+                print('g')
+                break
+            elif keyboard.is_pressed('h'):
+                print('h')
+                break
+            elif keyboard.is_pressed('i'):
+                print('i')
+                break
+            elif keyboard.is_pressed('j'):
+                print('j')
+                break
+            elif keyboard.is_pressed('k'):
+                print('k')
+                break
+            elif keyboard.is_pressed('l'):
+                print('L')
+                break
+            elif keyboard.is_pressed('m'):
+                print('m')
+                break
+            elif keyboard.is_pressed('n'):
+                print('n')
+                break
+            elif keyboard.is_pressed('o'):
+                print('o')
+                break
+            elif keyboard.is_pressed('p'):
+                print('p')
+                break
+            elif keyboard.is_pressed('q'):
+                print('q')
+                break
+            elif keyboard.is_pressed('r'):
+                print('r')
+                break
+            elif keyboard.is_pressed('s'):
+                print('s')
+                break
+            elif keyboard.is_pressed('t'):
+                print('t')
+                break
+            elif keyboard.is_pressed('u'):
+                print('u')
+                break
+            elif keyboard.is_pressed('v'):
+                print('v')
+                break
+            elif keyboard.is_pressed('w'):
+                print('w')
+                break
+            elif keyboard.is_pressed('x'):
+                print('X')
+                break
+            elif keyboard.is_pressed('w'):
+                print('w')
+                break
+            elif keyboard.is_pressed('z'):
+                print('z')
+                break
+            elif keyboard.is_pressed('1'):
+                print('1')
+                break
+            elif keyboard.is_pressed('2'):
+                print('2')
+                break
+            elif keyboard.is_pressed('3'):
+                print('3')
+                break
+            elif keyboard.is_pressed('4'):
+                print(4)
+                break
+            elif keyboard.is_pressed('5'):
+                print('5')
+                break
+            elif keyboard.is_pressed('6'):
+                print(6)
+                break
+            elif keyboard.is_pressed('7'):
+                print('7')
+                break
+            elif keyboard.is_pressed('8'):
+                print(8)
+                break
+            elif keyboard.is_pressed('9'):
+                print('9')
+                break
+            break
+
+keylogger()
+
+
+
